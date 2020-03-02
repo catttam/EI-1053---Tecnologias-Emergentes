db08f8346dc13106e7de85f7b67ee06737db636c
David Tortosa <davidtortosa@MacBook-Pro-de-DavidT.local>
2020-03-02 16:32:52 +0100

    clients server

--- server.js
+++ server.js
@@ -10,25 +10,29 @@ app.use(bodyparser.json())
 
 app.use(control.middleware) //Done
 
-app.use('/web', express.static('public'))
+//app.use('/web', express.static('public'))
 
-app.get('/news', control.eventStream) //Stream of Server-side events
+//app.get('/news', control.eventStream) //Stream of Server-side events
 
-app.get('/user/', control.getUsers)
 
-app.get('/user/:id', control.getUserData ) //Done
+//app.get('/user/:id', control.getUserData ) //Done
 
-app.post('/user', control.updateUser ) //Done
+//app.post('/user', control.updateUser ) //Done
 
-app.get('/blog', control.getBlogs ) //Done
+//app.get('/blog', control.getBlogs ) //Done
 
-app.post('/blog', control.createBlog ) //Done
+//app.post('/blog', control.createBlog ) //Done
 
-app.get('/blog/:id', control.getBlogData ) //Done
+//app.get('/blog/:id', control.getBlogData ) //Done
 
-app.get('/blog/search', control.searchBlog ) //Done
+//app.get('/blog/search', control.searchBlog ) //Done
 
 //etc ...
+
+app.get('/user/:client_id', control.getUser)
+app.post('/user/client', control.createUser)
+app.post('/user/client/update/:client_id', control.updateUser)
+app.delete('/user/client/delete/:client_id', control.deleteUser)
 
 const PORT = 8000
 app.listen(PORT, _ => console.log(`Servidor web escuchando en puerto ${PORT}`))
